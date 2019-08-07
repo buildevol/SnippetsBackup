32285a7b3687789ec3652afd216fb73c07b3ee4f
buildevol <buildevol@outlook.com>
2019-08-07 19:59:59 +0800

    Refactor code and implement calculation of number of items in all pages
    Implement calculation of total number of items in all pages
    Refactor the code for better readability and for reusability as an independent GitHub python module (library)

--- GitHub.py
+++ GitHub.py
@@ -4,10 +4,11 @@ Author: LEE WEI HAO JONATHAN (buildevol)
 """
 
 import requests
-from urllib.parse import urlparse
+from urllib.parse import urlparse, parse_qs
 
 GITHUB_API_BASE_URL = "https://api.github.com"
 GITHUB_API_DEFAULT_NUM_OF_PAGE_ITEMS = 30
+GITHUB_API_MAX_NUM_OF_PAGE_ITEMS = 100
 
 CHUNK_SIZE = 1024 * 1024    # 1 MB
 
@@ -26,7 +27,7 @@ def backup_single_github_gist():
     response = requests.get(api_github_url_format)
     response.raise_for_status()
     decoded_json_response = response.json()
-    raw_files_url_dict = get_raw_files_url_from_single_gist_dict(decoded_json_response)
+    raw_files_url_dict = get_raw_files_url_dict_from_single_gist(decoded_json_response)
 
     for file_url in raw_files_url_dict:
         file_response = requests.get(file_url)
@@ -48,9 +49,11 @@ def backup_github_gist_from_username():
     github_username = input("Enter a GitHub username: ")
 
     api_github_url_format = GITHUB_API_BASE_URL + "/users/" + github_username + "/gists"
-    response = requests.get(api_github_url_format)
+    response = requests.get(api_github_url_format, params={"per_page": GITHUB_API_MAX_NUM_OF_PAGE_ITEMS})
     response.raise_for_status()
     decoded_json_response = response.json()
+    total_num_of_github_gists = total_num_of_items_in_all_pages(response,
+                                                                custom_page_size=GITHUB_API_MAX_NUM_OF_PAGE_ITEMS)
 
 
 def parse_single_gist_url(raw_gist_url):
@@ -68,7 +71,7 @@ def parse_single_gist_url(raw_gist_url):
     return parsed_github_gist_url_list
 
 
-def get_raw_files_url_from_single_gist_dict(decoded_json):
+def get_raw_files_url_dict_from_single_gist(decoded_json):
     """
     Parses the input deserialised json to obtain a dict of raw files urls in the GitHub Gist URL.
     :param decoded_json: A deserialised json for a single GitHub Gist from the json returned by GitHub API.
@@ -83,12 +86,23 @@ def get_raw_files_url_from_single_gist_dict(decoded_json):
     return result
 
 
-def total_num_of_items_in_all_pages(link_header_in_response, custom_page_size=GITHUB_API_DEFAULT_NUM_OF_PAGE_ITEMS):
+def total_num_of_items_in_all_pages(response, custom_page_size=GITHUB_API_DEFAULT_NUM_OF_PAGE_ITEMS):
     """
     Calculates the total number of items in all pages based on the input link header response from GitHub API
     and the input custom page size.
-    :param link_header_in_response: The link header response returned by GitHub API.
-    :param custom_page_size: The custom page size whose default value is based on GITHUB_API_DEFAULT_NUM_OF_PAGE_ITEMS
+    :param response: The response returned by GitHub API.
+    :param custom_page_size: The custom page size whose default value is based on GITHUB_API_DEFAULT_NUM_OF_PAGE_ITEMS.
     :return: The total number of items in all pages.
     """
-    pass
+    link_header = response.links
+    if len(link_header) == 0:
+        return len(response.json())
+
+    last_page_url = link_header["last"]["url"]
+    last_page_response = requests.get(last_page_url)
+    query_string = urlparse(last_page_url).query
+    parsed_query_string = parse_qs(query_string)
+    last_page_num = int(parsed_query_string["page"][0])
+
+    return len(last_page_response.json()) + (int(custom_page_size) * (last_page_num - 1))
+
